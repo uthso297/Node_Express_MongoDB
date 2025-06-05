39d6952aa7070ee6da18527fedee561d7714cb1a
uthso297 <uthso119464@gmail.com>
2025-06-05 23:01:00 +0600

    updated .gitignore

--- EspressJSDay3/dist/App/config/mongodb.js
+++ EspressJSDay3/dist/App/config/mongodb.js
@@ -2,7 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.client = void 0;
 const mongodb_1 = require("mongodb");
-const uri = "mongodb+srv://uthsoDB:UthsoMef3647@cluster0.g3mp55c.mongodb.net/todosDB?retryWrites=true&w=majority&appName=Cluster0";
+require("dotenv/config");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g3mp55c.mongodb.net/todosDB?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 exports.client = new mongodb_1.MongoClient(uri, {
     serverApi: {
--- EspressJSDay3/src/App/config/mongodb.ts
+++ EspressJSDay3/src/App/config/mongodb.ts
@@ -1,7 +1,6 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
-
-
-const uri = "mongodb+srv://uthsoDB:UthsoMef3647@cluster0.g3mp55c.mongodb.net/todosDB?retryWrites=true&w=majority&appName=Cluster0";
+import 'dotenv/config'
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g3mp55c.mongodb.net/todosDB?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 export const client = new MongoClient(uri, {
     serverApi: {
